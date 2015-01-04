34343e0a57b679170887efed6e6dcfe56e138df8
donand <donati.andrea.00@gmail.com>
2015-01-04 23:50:44 +0100

    Merge branch 'master' into Route-HTTP-POST
    Conflicts:
    	app/src/main/java/com/mobike/mobike/SummaryActivity.java

--- app/src/main/java/com/mobike/mobike/SummaryActivity.java
+++ app/src/main/java/com/mobike/mobike/SummaryActivity.java
@@ -1,6 +1,8 @@
 package com.mobike.mobike;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
